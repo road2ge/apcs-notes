3383b6a8df0847b8e11dc222a6a9c8da888a4f21
Kale Miller <kalemiller@simplecyberspace.com>
2017-09-28 09:54:29 -0400

    kappa kappa kappa
    kappa kappa kapp
    kappa kappa kappa, kappa kappa kappa, kap kap, kap, a!

--- Unit2/Program2.java
+++ Unit2/Program2.java
@@ -1,33 +1,23 @@
 
 /**
- * Write a description of class Program2 here.
+ * omfg why
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Kale Miller
+ * @version 9/28/2017
  */
+import java.util.*;
 public class Program2
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Program2
-     */
-    public Program2()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+   public static void main(String[] args)
+   {
+       Scanner scan = new Scanner(System.in);
+       double sum = 0.0;
+       for(int i = 0; i < 3; i++)
+       {
+           System.out.println("Type a number and then hit enter");
+           sum = scan.nextDouble();
+       }
+       double average = sum/3;
+       System.out.println("The average of those three numbers is " + average);
+   }
 }
