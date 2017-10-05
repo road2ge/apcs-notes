712080332443951900291a4b21f27f05c281ec65
Titan - KittyS <kalemiller@simplecyberspace.com>
2017-10-04 22:51:38 -0400

    uhm

--- Unit2/Program10.java
+++ Unit2/Program10.java
@@ -1,7 +1,23 @@
+/**
+ * Uses Heron's formula to calculate are from  given triangle dimensions.
+ * The triangle is not guaranteed to be possible.
+ *
+ * @author Kale Miller
+ * @version 10/4/17
+ */
+import java.util.*;
 public class Program10
 {
     public static void main(String[] args)
     {
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("You will input the dimensions of a triangle, as positive integers, pressing enter after each.");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        double s = (a+b+c)/2;
+        double area = Math.sqrt((s*(s-a)*(s-b)*(s-c)));
+        System.out.println("The area of that triangle is " + area);
     }
 }
